fd87fde997c501d68b15eabd921d71c3373dd2ac
Khamzat Yerzhanov <h.yerzhanov@gmail.com>
2021-06-30 23:34:48 +0600

    init

--- app/src/main/java/com/example/stanrageapp/MainActivity.kt
+++ app/src/main/java/com/example/stanrageapp/MainActivity.kt
@@ -27,11 +27,6 @@ class MainActivity : AppCompatActivity() {
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
 
-        this.window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                or View.SYSTEM_UI_FLAG_FULLSCREEN
-                or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY)
+        supportActionBar?.hide()
     }
 }
