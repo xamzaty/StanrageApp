6c9b8193873f5330029e3d0bfad288952c4f997e
Khamzat Yerzhanov <h.yerzhanov@gmail.com>
2021-07-03 10:29:33 +0600

    Добавлены новые элементы

--- app/src/main/java/com/example/stanrageapp/ui/notifications/NotificationsFragment.kt
+++ app/src/main/java/com/example/stanrageapp/ui/notifications/NotificationsFragment.kt
@@ -4,26 +4,48 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Spinner
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.GridLayoutManager
 import com.example.stanrageapp.R
+import com.example.stanrageapp.base.BaseFragment
+import com.example.stanrageapp.databinding.FragmentNotificationsBinding
+import com.skydoves.powerspinner.IconSpinnerAdapter
+import com.skydoves.powerspinner.IconSpinnerItem
+import com.skydoves.powerspinner.SpinnerAnimation
 
-class NotificationsFragment : Fragment() {
+class NotificationsFragment : BaseFragment<FragmentNotificationsBinding>(FragmentNotificationsBinding::inflate) {
 
-    private lateinit var notificationsViewModel: NotificationsViewModel
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
-    override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
-    ): View? {
-        notificationsViewModel =
-                ViewModelProvider(this).get(NotificationsViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_notifications, container, false)
-        notificationsViewModel.text.observe(viewLifecycleOwner, Observer {
-        })
-        return root
+        spinnerView()
+    }
+
+    private fun spinnerView() {
+        SpinnerAnimation.FADE
+
+        binding.apply {
+            val adapter = IconSpinnerAdapter(spinner)
+            spinner.setSpinnerAdapter(adapter)
+            spinner.getSpinnerRecyclerView().layoutManager = GridLayoutManager(context, 2)
+
+        }
+
+        binding.spinner.apply {
+            setSpinnerAdapter(IconSpinnerAdapter(this))
+            setItems(
+                arrayListOf(
+                    IconSpinnerItem(text = "30", iconRes = R.drawable.ic_baseline_adjust_24),
+                    IconSpinnerItem(text = "60", iconRes = R.drawable.ic_baseline_adjust_24),
+                    IconSpinnerItem(text = "90", iconRes = R.drawable.ic_baseline_adjust_24)))
+            getSpinnerRecyclerView().layoutManager = GridLayoutManager(context, 1)
+            selectItemByIndex(0)
+            lifecycleOwner = activity
+        }
+
     }
 }
